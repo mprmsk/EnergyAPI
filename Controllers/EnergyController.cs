17a642d26785d67f4047ff8979995ccc987a39b7
mprmsk <99753265+mprmsk@users.noreply.github.com>
2022-02-15 22:57:27 +0530

    Add files via upload

---
+++ Controllers/EnergyController.cs
@@ -0,0 +1,38 @@
+﻿using EnergyAPI.DateCalcBusinessObjects;
+using EnergyAPI.Energy.BO.DataSerialize;
+using EnergyAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace EnergyAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EnergyController : ControllerBase
+    {
+        private readonly IEnergyCalcBO energy;
+        private readonly IDataSerialize dataSerialize;
+        private readonly string path = @"C:\Temp.json";
+
+        public EnergyController(IEnergyCalcBO _energy, IDataSerialize _dataSerialize)
+        {
+            this.energy = _energy;
+            this.dataSerialize = _dataSerialize;
+        }
+        [HttpPost]
+        public IList<EnergyModel> Post(EnergyModel model) 
+        {
+            List<EnergyModel> listDetails = null;
+            if (this.energy.CalculateWeekEnd(model.EnteredDate)) {
+                model.Cost = model.Cost / 10;
+            }
+
+            this.dataSerialize.DataSerializetoJson(path, model);
+            listDetails = this.dataSerialize.DataSerializeFromJson(path, model); 
+
+            return listDetails;
+        }
+
+    }
+
+}
---
+++ Energy.BO/DataSerialize/DataSerialize.cs
@@ -0,0 +1,28 @@
+﻿using EnergyAPI.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnergyAPI.Energy.BO.DataSerialize
+{
+    public class DataSerialize : IDataSerialize
+    {
+        public List<EnergyModel> DataSerializeFromJson(string path, EnergyModel model)
+        {
+            StreamReader reader = new StreamReader(path);
+            return JsonConvert.DeserializeObject<List<EnergyModel>>(reader.ReadToEnd());
+        }
+
+        public void DataSerializetoJson(string path, EnergyModel model)
+        {
+            TextWriter writer;
+            using (writer = new StreamWriter(path, append: true))
+            {
+                writer.WriteLine(JsonConvert.SerializeObject(model));
+            }
+        }
+    }
+}
